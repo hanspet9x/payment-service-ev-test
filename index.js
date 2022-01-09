a62da7e142a60fea4b201cae8f5b2ee5f3b7b305
hanspet9x <peterakinlolu1@gmail.com>
2022-01-09 17:01:45 +0100

    updated post

--- index.js
+++ index.js
@@ -4,23 +4,31 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.text());
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
-})
-app.post('/form', (req, res) => {
-    const data = JSON.stringify(req.body) + "\n\n";
-    console.log(data);
-    fs.appendFile('file.txt', data, (err) => {
-        console.log(err);
-    });
+});
 
-    res.status(200).send(data);
+app.post('/form', (req, res) => {
+    const body = req.body;
+    if (body) {
+        let data = typeof body === 'string' ? body : JSON.parse(body);
+        fs.appendFile('file.txt', data + "\n\n", (err) => {
+            if(err) {
+                res.status(200).send(err.message);
+                return;
+            }
+            res.status(200).send(data);
+        });
+    }
+
+    res.send('no valid data');
 })
 
 app.get('data', (req, res) => {
     fs.readFile('file.txt', (err, data) => {
-        if(err) {
+        if (err) {
             res.send(err.message);
             return;
         }
