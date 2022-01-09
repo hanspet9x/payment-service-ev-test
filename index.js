d7d7aeb6cd185490d135073bdd87efa0f14d4f58
hanspet9x <peterakinlolu1@gmail.com>
2022-01-09 15:53:59 +0100

    deployed

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 
 app.use(express.json());
 
-app.post('form', (req, res) => {
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+app.post('/form', (req, res) => {
     const data = JSON.stringify(req.body) + "\n\n";
     console.log(data);
     fs.appendFile('file.txt', data, (err) => {
